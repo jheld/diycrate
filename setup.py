8d87a5fe4f24ce3f5b6a5308da2c6592aa7890dc
Jason Held <jasonsheld@gmail.com>
2016-07-31 15:19:42 -0400

    start down/upload mon threads with start cloud threads only
    add new ipython and boxsdk

--- setup.py
+++ setup.py
@@ -18,14 +18,12 @@ def read(file_name):
 
 
 packages = ['bottle==0.12.9',
-            'boxsdk==1.5.1',
+            'boxsdk==1.5.3',
             'decorator==4.0.9',
             'enum34==1.1.2',
-            'ipython==4.1.2',
-            'ipython-genutils==0.1.0',
+            'ipython>=5',
             'path.py==8.1.2',
             'pexpect==4.0.1',
-            'pickleshare==0.6',
             'ptyprocess==0.5.1',
             'pyinotify==0.9.6',
             'redis==2.10.5',
@@ -33,8 +31,6 @@ packages = ['bottle==0.12.9',
             'requests-toolbelt==0.6.0',
             'simplegeneric==0.8.1',
             'six==1.10.0',
-            'traitlets==4.2.1',
-            'wheel==0.29.0',
             'pyopenssl==16.0.0',
             'cherrypy==5.1.0']
 
