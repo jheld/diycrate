44d77910e8ac89ff1d80f41e71ddface24d79416
Jason Held <jasonsheld@gmail.com>
2020-07-18 16:35:21 -0400

    0.2.11.0rc3

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ packages = [
 
 setup(
     name="diycrate",
-    version="0.2.11.0rc2",
+    version="0.2.11.0rc3",
     author="Jason Held",
     author_email="jasonsheld@gmail.com",
     description="box.com for linux -- unofficial, based on python SDK",
